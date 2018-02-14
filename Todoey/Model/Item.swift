31c7f783e45bfddffaa1f42a365146e2f964727d
Fotis Mitropoulos <fotismitropoulos@gmail.com>
2018-02-14 15:16:48 +0200

    Conver to MVC design Pattern

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Fotis Mitropoulos on 14/2/18.
+//  Copyright © 2018 fotismitropoulos. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
